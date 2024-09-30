d8023c4bc394d7466d980cbbc839434ed17fced1
Miika Jukakoski <miika.jukakoski@teknoware.com>
2024-09-30 14:51:29 +0300

    new try

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -24,8 +24,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "028cdb9d815cee3bf5762aedd42eeae8
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e7b5e4fbe0e3a033a234ff6aa4a1e22a",
-"/": "e7b5e4fbe0e3a033a234ff6aa4a1e22a",
+"index.html": "a56d8c1b90ee8f30d0df7dbc4eb14826",
+"/": "a56d8c1b90ee8f30d0df7dbc4eb14826",
 "main.dart.js": "c5181de7660567e3a324b7e9a1b2cd21",
 "manifest.json": "2f8b56aa66633aa5c12778491ddb2701",
 "version.json": "53de3fef74411db22e7a95509d5574c7"};
